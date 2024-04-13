25ee50f87d93c22e1d32b2104600e6a0d8567968
ErvinDimitri <ervinluis84@gmail.com>
2024-04-13 20:10:47 +0200

    Feature: Update APIs calls and vars

--- Week3_React/src/App.js
+++ Week3_React/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import UserList from './components/UserList';
+import UsersListWithHook from './components/UsersListWithHook';
+import UserForm from './components/UserForm';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Route path="/" exact component={Home} />
       <Route path="/contact" exact component={Contact} />
       <Route path="/users" exact component={UserList} />
+      <Route path="/hook-fetch-users" component={UsersListWithHook} />
+      <Route path="/create-user"  component={UserForm} />
       </Switch>    
     </BrowserRouter>
   )
--- Week3_React/src/components/UserList.js
+++ Week3_React/src/components/UserList.js
@@ -1,47 +1,53 @@
-import React, {useState, useEffect} from 'react'
+// we are going to fetch a list of users from a dummy api(JsonPlaceholder) -> list of users and render them on UI
 
-const UserList = (props) => {
-  const [users, setUsers ] = useState([
-    // {
-    //   name: "Ervin",
-    //   email: "ervin@mail.com"
-    // }
-  ])
 
-  useEffect(()=>{
-    const fetchUsers = async ()=>{
+import React,{useState,useEffect} from 'react'
+
+
+const UsersList = (props) => {
+
+  const [users,setUsers] = useState([])
+  
+  useEffect(() => {
+    //api calling logic and setUsers()
+    const fetchUsers = async () => {
       try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        //calling api
+        
+        const response = await fetch('http://localhost:8080/employee/_search')
+
         if(!response.ok){
           throw new Error('Failed to fetch users')
         }
-        const data = await response.json()
 
-        setUsers(data) 
-     }
-      catch(error){
-        console.error('error fetching data:', error)
+        const data = await response.json()
+        setUsers(data)
+        
+      }
+      catch(error) {
+        console.error('Error fetching data:',error)
       }
     }
     fetchUsers()
   },[])
   
+
   return (
     <div className='user-list-container'>
-      <div className='list-title'>User List</div>
-        <ul className='user-list'>
-          {
-            users.map((user,idx) => {
-              return(
-                <li key={idx} className='user-item' >
-                  <strong>{user.name}</strong> - {user.email}
-                </li>
-              )
-            })
-          }
-        </ul>
+      <h1 className='list-title'>User List</h1>
+      <ul className='user-list'>
+        {
+          users.map((user,idx) => {
+            return (
+              <li key={idx} className='user-item'>
+                <strong>{user.firstName} {user.lastName}</strong> - {user.emailId}
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
 
-export default UserList
+export default UsersList
